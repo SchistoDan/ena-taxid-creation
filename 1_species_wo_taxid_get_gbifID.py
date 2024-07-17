ab4343b68502f72f4e5fbc16b6303f1dc075bcc4
Dan Parsons <108939145+SchistoDan@users.noreply.github.com>
2024-07-17 13:06:59 +0100

    Update 1_species_wo_taxid_get_gbifID.py

--- 1_species_wo_taxid_get_gbifID.py
+++ 1_species_wo_taxid_get_gbifID.py
@@ -16,13 +16,15 @@ import os
 import pandas as pd
 from pygbif import species #NEEDS pygbif INSTALLED IN CONDA ENV TO WORK
 
+
+
 def tax_request(input_csv, output_tsv):
     df = pd.read_csv(input_csv)
     
-    #filter rows where 'matched_rank' does not equal 'species'
+#filter rows where 'matched_rank' does not equal 'species'
     filtered_df = df[df['matched_rank'].str.lower() != 'species']
     
-    #select necessary columns and rename them
+#select necessary columns and rename them
     selected_columns = {
         'taxid': 'proposed_name',  # Rename 'taxid' to 'proposed_name'
         'Species': 'name_type'  # Rename 'Species' to 'name_type'
@@ -31,25 +33,23 @@ def tax_request(input_csv, output_tsv):
     output_df = filtered_df[list(selected_columns.keys())].copy()
     output_df.rename(columns=selected_columns, inplace=True)
     
-    #convert 'proposed_name' to integer to remove decimals and handle NaN
+#convert 'proposed_name' to integer to remove decimals and handle NaN
     output_df['proposed_name'] = pd.to_numeric(output_df['proposed_name'], errors='coerce').astype('Int64')
     
-    #add new columns 'host', 'project_id', and 'description'
+#add new columns 'host', 'project_id', and 'description'
     output_df['host'] = ''  # Empty column - not needed
     output_df['project_id'] = 'BGE'  # Populate 'project_id' with 'BGE' string
     output_df['description'] = ''  # Will be populated with GBIF ID
-        
-    #reorder columns as in tempalte tax_request.tsv
+   
     output_df = output_df[['proposed_name', 'name_type', 'host', 'project_id', 'description']]
     
-    #save filtered DataFrame to temp TSV file
+#save filtered DataFrame to temp TSV file
     temp_tsv = 'temp_taxonomy_request.tsv'
     output_df.to_csv(temp_tsv, sep='\t', index=False)
     
-    #resolve names using GBIF and update the 'description' column
+#resolve names using GBIF and update the 'description' column
     resolve_names_and_update_file(temp_tsv, output_tsv)
 
-    #remove temp file
     os.remove(temp_tsv)
     print(f"Species without species-level taxids saved to {output_tsv}")
 
@@ -59,35 +59,34 @@ def tax_request(input_csv, output_tsv):
 def resolve_names_and_update_file(input_filename, output_filename):
     input_df = pd.read_csv(input_filename, sep='\t')
 
-    #ensure the description column is treated as string type
+#ensure the description column is treated as string type
     input_df['description'] = input_df['description'].astype(str)
 
-    #check if the required column exists
+#check if the required column exists
     if 'name_type' not in input_df.columns:
         print("Error: The input file must contain a 'name_type' column.")
         sys.exit(1)
 
     results = []
 
-    #iterate over each row in the DataFrame
+#iterate over each row in the DataFrame
     for index, row in input_df.iterrows():
         name_info = {'name': row['name_type']}
 
         try:
-            #perform taxonomic name resolution using species.name_backbone
+#perform taxonomic name resolution using species.name_backbone
             result = species.name_backbone(**name_info)
-            result['index'] = index  # Keep track of the original index
-            #append to results list
+            result['index'] = index  #keep track of the original index
+#append to results list
             results.append(result)
         except Exception as e:
-            #handle any exceptions that occur and append the error message to the results
+#handle any exceptions and append error message to results
             print(f"Error resolving name: {name_info}. Error: {e}")
             results.append({"Error": str(e), **name_info, "index": index})
 
-    #convert results to DataFrame
     result_df = pd.DataFrame(results)
 
-    #filter results based on GBIF - consistent = ID confidence > 95% ,status = ACCEPTED, and class == Insecta, otherwise = inconsistent. 
+#filter results based on GBIF - consistent = ID confidence > 95% ,status = ACCEPTED, and class == Insecta, otherwise = inconsistent. 
     consistent_df = result_df[(result_df['confidence'] > 95) & 
                               (result_df['status'] == 'ACCEPTED') & 
                               (result_df['class'] == 'Insecta') &
@@ -98,7 +97,8 @@ def resolve_names_and_update_file(input_filename, output_filename):
                                 (result_df['class'] != 'Insecta') |
                               (result_df['matchType'] != 'EXACT')]
 
-    #update the 'description' column in the input DataFrame
+
+#update the 'description' column in the input DataFrame
     for index, row in consistent_df.iterrows():
         # Ensure the 'description' column does not contain 'nan' strings
         current_description = input_df.at[row['index'], 'description']
@@ -107,18 +107,19 @@ def resolve_names_and_update_file(input_filename, output_filename):
         else:
             current_description = str(current_description)
         
-        # Append the speciesKey to the description
+#append speciesKey to description column
         species_key = int(row['speciesKey'])  # Ensure speciesKey is an integer
         input_df.at[row['index'], 'description'] = f"{current_description} {species_key}".strip()
 
-    # Write the updated input DataFrame to the output TSV file
+#write updated df to the output TSV 
     input_df.to_csv(output_filename, sep='\t', index=False, float_format='%d')
     print(f"Updated file written to {output_filename}")
 
-    # Write the inconsistent rows to a TSV file
+#write inconsistent GBIF rows based on filtering criteria to TSV 
     inconsistent_filename = "gbif_inconsistent.tsv"
     inconsistent_df.to_csv(inconsistent_filename, sep='\t', index=False, float_format='%d')
     print(f"Inconsistent rows written to {inconsistent_filename}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
